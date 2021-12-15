08d48fed50be8051ec7a5f7fed4f8c505f56f36b
christopher-besch <christopher.besch@gmx.de>
2021-12-15 20:59:36 +0100

    some updates

--- config.lua
+++ config.lua
@@ -87,15 +87,15 @@ lvim.builtin.which_key.vmappings["s"] = { "<cmd>'<,'>s/ss/ß/g<CR>", "Replace ss
 lvim.builtin.which_key.mappings["u"] = { "/ae\\|oe\\|ue\\|ss<CR>", "Find Umlauts" }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
--- lvim.builtin.dashboard.active = true
--- lvim.builtin.terminal.active = true
--- lvim.builtin.nvimtree.setup.view.side = "left"
--- lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
--- lvim.builtin.treesitter.ensure_installed = "maintained"
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
--- lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
 
 -- lvim.autocommands.custom_groups = {
 --     -- set sensible c++ comments
